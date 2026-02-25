4591fff89883af2c6b39641633d2a8243796b71c
calebvenable <caleb.venable@dorsetsoftware.com>
2026-02-25 15:18:49 +0000

    Rename /home route to /

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -44,7 +44,7 @@ export function SignUpForm({
         email,
         password,
         options: {
-          emailRedirectTo: `${window.location.origin}/home`,
+          emailRedirectTo: `${window.location.origin}/`,
         },
       });
       if (error) throw error;

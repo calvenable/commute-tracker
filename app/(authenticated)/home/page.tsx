efb66bcc93f8a19182e4c96d303659517e4c2c7c
calebvenable <caleb.venable@dorsetsoftware.com>
2026-02-25 13:02:35 +0000

    Restructure files to allow authenticated page layout

---
+++ app/(authenticated)/home/page.tsx
@@ -0,0 +1,23 @@
+
+import BottomNav from "@/components/bottom-nav";
+import { Page } from "@/types/Page";
+import { InfoIcon } from "lucide-react";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex-1 w-full flex flex-col gap-12">
+        <div className="w-full">
+          <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
+            <InfoIcon size="16" strokeWidth={2} />
+            You are on the Homepage
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 items-start">
+
+        </div>
+      </div>
+      <BottomNav selectedPage={Page.HOME}/>
+    </>
+  );
+}
---
+++ types/Page.tsx
@@ -0,0 +1,6 @@
+export enum Page {
+    HOME,
+    RECORD,
+    ZONES,
+    SETTINGS
+}
